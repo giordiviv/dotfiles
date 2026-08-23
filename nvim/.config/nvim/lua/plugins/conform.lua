989135c6d688d907efbd044917b92af3ff490dcd
giordiviv <224191292+giordiviv@users.noreply.github.com>
2026-08-23 03:44:54 +0200

    Stylua should now allow for spaces and file was formatted

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,23 +1,30 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" }, -- lazy-load right before the first save
-  cmd = { "ConformInfo" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    })
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" }, -- lazy-load right before the first save
+	cmd = { "ConformInfo" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
 
-    -- Manual format keymap (normal + visual), in case you want to
-    -- format without saving.
-    vim.keymap.set({ "n", "v" }, "<leader>f", function()
-      require("conform").format({ async = true, lsp_format = "fallback" })
-    end, { desc = "Format buffer" })
-  end,
+		-- Pass StyLua flags:
+		formatters =
+			{
+				stylua = {
+					prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+				},
+			},
+			-- Manual format keymap (normal + visual), in case you want to
+			-- format without saving.
+			vim.keymap.set({ "n", "v" }, "<leader>f", function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end, { desc = "Format buffer" })
+	end,
 }
