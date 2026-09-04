f9950b46c88fcb1fb82cafeed4fd26825e9a0472
giordiviv <224191292+giordiviv@users.noreply.github.com>
2026-09-04 06:15:46 +0200

    Modified lualine to contain search results, last message and the command.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -10,8 +10,39 @@ return {
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { { "filename", path = 1 } },
-                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_c = { { "filename", path = 0 } },
+                lualine_x = {
+                    ---@diagnostic disable: undefined-field
+                    {
+                        function()
+                            return require("noice").api.status.message.get_hl()
+                        end,
+                        cond = function()
+                            return require("noice").api.status.message.has()
+                        end,
+                    },
+                    {
+                        function()
+                            return require("noice").api.status.command.get()
+                        end,
+                        cond = function()
+                            return require("noice").api.status.command.has()
+                        end,
+                        color = { fg = "#ff9e64" },
+                    },
+                    {
+                        function()
+                            return require("noice").api.status.search.get()
+                        end,
+                        cond = function()
+                            return require("noice").api.status.search.has()
+                        end,
+                        color = { fg = "#ff9e64" },
+                    },
+                    "encoding",
+                    "fileformat",
+                    "filetype",
+                },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
             },
